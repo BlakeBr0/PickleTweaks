c39bf9cc0acbbce34ec95e3409f9b2b9a34df162
Unknown <luasmrks@gmail.com>
2018-01-25 16:48:10 -0600

    Allow dye powders to dye sheep

--- 1.12/src/main/java/com/blakebr0/pickletweaks/feature/item/ItemDyePowder.java
+++ 1.12/src/main/java/com/blakebr0/pickletweaks/feature/item/ItemDyePowder.java
@@ -6,7 +6,12 @@ import com.blakebr0.pickletweaks.PickleTweaks;
 import com.blakebr0.pickletweaks.config.ModConfig;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.client.model.ModelLoader;
 
 public class ItemDyePowder extends ItemMeta implements IEnableable {
@@ -15,6 +20,23 @@ public class ItemDyePowder extends ItemMeta implements IEnableable {
 		super("pt.dye_powder", PickleTweaks.REGISTRY);
 		this.setCreativeTab(PickleTweaks.tab);
 	}
+	
+	@Override
+    public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer player, EntityLivingBase target, EnumHand hand) {
+        if (target instanceof EntitySheep) {
+            EntitySheep sheep = (EntitySheep) target;
+            EnumDyeColor color = EnumDyeColor.byMetadata(stack.getMetadata());
+
+            if (!sheep.getSheared() && sheep.getFleeceColor() != color) {
+                sheep.setFleeceColor(color);
+                stack.shrink(1);
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 	@Override
 	public void init() {
