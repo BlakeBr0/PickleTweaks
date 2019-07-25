242d122d978d9bc21e94bf1e45738be86efd1ea0
BlakeBr0 <luasmrks@gmail.com>
2019-07-24 23:54:49 -0500

    Fix paxels not working on all blocks

--- src/main/java/com/blakebr0/pickletweaks/feature/item/PaxelItem.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/item/PaxelItem.java
@@ -2,7 +2,12 @@ package com.blakebr0.pickletweaks.feature.item;
 
 import com.blakebr0.cucumber.iface.IEnableable;
 import com.blakebr0.pickletweaks.config.ModConfigs;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
 import net.minecraftforge.common.ToolType;
 
@@ -17,6 +22,24 @@ public class PaxelItem extends ToolItem implements IEnableable {
 				.addToolType(ToolType.SHOVEL, tier.getHarvestLevel())
 				.addToolType(ToolType.AXE, tier.getHarvestLevel())
 		));
+	}
+
+	public boolean canHarvestBlock(BlockState state) {
+		Block block = state.getBlock();
+		int i = this.getTier().getHarvestLevel();
+		if (state.getHarvestTool() == ToolType.PICKAXE)
+			return i >= state.getHarvestLevel();
+
+		Material material = state.getMaterial();
+		return material == Material.ROCK || material == Material.IRON || material == Material.ANVIL
+				|| block == Blocks.SNOW || block == Blocks.SNOW_BLOCK;
+	}
+
+	public float getDestroySpeed(ItemStack stack, BlockState state) {
+		Material material = state.getMaterial();
+		return material != Material.IRON && material != Material.ANVIL && material != Material.ROCK
+				&& material != Material.WOOD && material != Material.PLANTS && material != Material.TALL_PLANTS
+				&& material != Material.BAMBOO ? super.getDestroySpeed(stack, state) : this.efficiency;
 	}
 
 	@Override
