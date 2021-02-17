58e5755297ab459a6870f99cd722bfaeed464b49
BlakeBr0 <luasmrks@gmail.com>
2021-02-16 23:57:52 -0600

    Change ammo tooltip for bows with infinity makes it so instead of having an ammo count it just makes the 'Infinity' text green or red

--- src/main/java/com/blakebr0/pickletweaks/feature/FeatureBowInfo.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/FeatureBowInfo.java
@@ -2,12 +2,17 @@ package com.blakebr0.pickletweaks.feature;
 
 import com.blakebr0.pickletweaks.config.ModConfigs;
 import com.blakebr0.pickletweaks.lib.ModTooltips;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArrowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShootableItem;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -16,33 +21,53 @@ import java.util.ListIterator;
 public final class FeatureBowInfo {
 	@SubscribeEvent
 	public void onTooltip(ItemTooltipEvent event) {
-		if (!ModConfigs.ENABLE_TOOL_INFO_TOOLTIP.get()) return;
+		if (!ModConfigs.ENABLE_TOOL_INFO_TOOLTIP.get())
+			return;
 
 		ItemStack stack = event.getItemStack();
 		ListIterator<ITextComponent> tooltip = event.getToolTip().listIterator();
 
 		Item item = stack.getItem();
 		if (item instanceof ShootableItem) {
-			tooltip.next();
+			PlayerEntity player = event.getPlayer();
 
-      		tooltip.add(ModTooltips.AMMO.args(this.getAmmo(event.getPlayer())).build());
+			if (EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack) > 0) {
+				while (tooltip.hasNext()) {
+					ITextComponent next = tooltip.next();
+
+					if (next instanceof TranslationTextComponent) {
+						String key = ((TranslationTextComponent) next).getKey();
+
+						if ("enchantment.minecraft.infinity".equals(key)) {
+							TextFormatting formatting = getAmmo(player) > 0 ? TextFormatting.GREEN : TextFormatting.RED;
+
+							tooltip.set(new StringTextComponent(next.getString()).mergeStyle(formatting));
+						}
+					}
+				}
+			} else {
+				tooltip.next();
+
+				int ammo = getAmmo(player);
+
+				tooltip.add(ModTooltips.AMMO.args(ammo).build());
+			}
 		}
 	}
 
-	public int getAmmo(PlayerEntity player) {
+	public static int getAmmo(PlayerEntity player) {
 		int ammo = 0;
 
-		if (player == null) return ammo;
+		if (player == null)
+			return ammo;
 
 		ItemStack offHand = player.inventory.offHandInventory.get(0);
-		if (offHand.getItem() instanceof ArrowItem) {
+		if (offHand.getItem() instanceof ArrowItem)
 			ammo += offHand.getCount();
-		}
 
 		for (ItemStack stack : player.inventory.mainInventory) {
-			if (stack.getItem() instanceof ArrowItem) {
+			if (stack.getItem() instanceof ArrowItem)
 				ammo += stack.getCount();
-			}
 		}
 
 		return ammo;
