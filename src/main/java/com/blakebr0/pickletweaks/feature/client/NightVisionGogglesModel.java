577bdbd128eec06a90b207bd90f92707544da093
BlakeBr0 <luasmrks@gmail.com>
2021-02-17 22:43:43 -0600

    Fix Night Vision Goggles model not rotating properly

--- src/main/java/com/blakebr0/pickletweaks/feature/client/NightVisionGogglesModel.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/client/NightVisionGogglesModel.java
@@ -16,7 +16,7 @@ public class NightVisionGogglesModel extends BipedModel<LivingEntity> {
         super(RenderType::getEntityTranslucent, 0.0F, 0.0F, 64, 32);
 
         this.main = new ModelRenderer(this, 0, 7);
-        this.main.setRotationPoint(0.0F, 0F, 0.0F);
+        this.main.setRotationPoint(0.0F, 0.0F, 0.0F);
         this.main.addBox(-8.0F, -7.0F, 1.0F, 1, 1, 6, 0.0F);
         this.main.addBox(7.0F, -7.0F, 1.0F, 1, 1, 6, 0.0F);
         this.main.addBox(-7.0F, -7.0F, 7.0F, 14, 1, 1, 0.0F);
@@ -28,22 +28,24 @@ public class NightVisionGogglesModel extends BipedModel<LivingEntity> {
 
         ModelRenderer left = new ModelRenderer(this, 6, 0);
         left.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.lens.addChild(left);
         left.addBox(-6.0F, -9.0F, -7.0F, 4, 5, 1, 0.0F);
         left.addBox(-2.0F, -8.0F, -7.0F, 1, 3, 1, 0.0F);
         left.addBox(-7.0F, -8.0F, -7.0F, 1, 4, 1, 0.0F);
         left.addBox(-6.0F, -4.0F, -7.0F, 3, 1, 1, 0.0F);
 
+        this.lens.addChild(left);
+
         ModelRenderer right = new ModelRenderer(this, 6, 0);
         right.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.lens.addChild(right);
         right.addBox(6.0F, -8.0F, -7.0F, 1, 4, 1, 0.0F);
         right.addBox(1.0F, -8.0F, -7.0F, 1, 3, 1, 0.0F);
         right.addBox(2.0F, -9.0F, -7.0F, 4, 5, 1, 0.0F);
         right.addBox(3.0F, -4.0F, -7.0F, 3, 1, 1, 0.0F);
 
+        this.lens.addChild(right);
+
         this.frame = new ModelRenderer(this, 0, 13);
-        this.frame.setRotationPoint(0.0F, 0F, 0.0F);
+        this.frame.setRotationPoint(0.0F, 0.0F, 0.0F);
         this.frame.addBox(-1.0F, -8.0F, -8.0F, 2, 3, 2, 0.0F);
         this.frame.addBox(-2.0F, -9.0F, -8.0F, 4, 1, 2, 0.0F);
         this.frame.addBox(-2.0F, -5.0F, -8.0F, 4, 1, 2, 0.0F);
@@ -65,35 +67,13 @@ public class NightVisionGogglesModel extends BipedModel<LivingEntity> {
     public void render(MatrixStack stack, IVertexBuilder buffer, int light, int overlay, float r, float g, float b, float a) {
         stack.push();
         stack.scale(0.6F, 0.6F, 0.6F);
-        this.main.render(stack, buffer, light, overlay);
-        this.lens.render(stack, buffer, light, overlay);
-        this.frame.render(stack, buffer, light, overlay);
-        stack.pop();
-    }
 
-    @Override
-    public void setRotationAngles(LivingEntity entity, float f1, float f2, float f3, float netHeadYaw, float headPitch) {
-        this.main.rotateAngleY = netHeadYaw * 0.017453292F;
-        this.lens.rotateAngleY = netHeadYaw * 0.017453292F;
-        this.frame.rotateAngleY = netHeadYaw * 0.017453292F;
-        if (entity.getTicksElytraFlying() > 4) {
-            this.main.rotateAngleX = -0.7853982F;
-            this.lens.rotateAngleX = -0.7853982F;
-            this.frame.rotateAngleX = -0.7853982F;
-        } else if (this.swimAnimation > 0.0F) {
-            if (entity.isActualySwimming()) {
-                this.main.rotateAngleX = this.rotLerpRad(this.main.rotateAngleX, -0.7853982F, this.swimAnimation);
-                this.lens.rotateAngleX = this.rotLerpRad(this.lens.rotateAngleX, -0.7853982F, this.swimAnimation);
-                this.frame.rotateAngleX = this.rotLerpRad(this.frame.rotateAngleX, -0.7853982F, this.swimAnimation);
-            } else {
-                this.main.rotateAngleX = this.rotLerpRad(this.main.rotateAngleX, headPitch * 0.017453292F, this.swimAnimation);
-                this.lens.rotateAngleX = this.rotLerpRad(this.lens.rotateAngleX, headPitch * 0.017453292F, this.swimAnimation);
-                this.frame.rotateAngleX = this.rotLerpRad(this.frame.rotateAngleX, headPitch * 0.017453292F, this.swimAnimation);
-            }
-        } else {
-            this.main.rotateAngleX = headPitch * 0.017453292F;
-            this.lens.rotateAngleX = headPitch * 0.017453292F;
-            this.frame.rotateAngleX = headPitch * 0.017453292F;
-        }
+        this.bipedHead.translateRotate(stack);
+
+        this.main.render(stack, buffer, light, overlay, r, g, b, a);
+        this.lens.render(stack, buffer, light, overlay, r, g, b, a);
+        this.frame.render(stack, buffer, light, overlay, r, g, b, a);
+
+        stack.pop();
     }
 }
