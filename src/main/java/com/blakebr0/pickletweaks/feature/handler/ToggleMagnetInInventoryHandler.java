b0e741e20fc51a0bf86ef38da064f3f48a06ec28
BlakeBr0 <luasmrks@gmail.com>
2021-03-28 20:40:19 -0500

    Play a ding sound when toggling the magnet

--- src/main/java/com/blakebr0/pickletweaks/feature/handler/ToggleMagnetInInventoryHandler.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/handler/ToggleMagnetInInventoryHandler.java
@@ -1,5 +1,6 @@
 package com.blakebr0.pickletweaks.feature.handler;
 
+import com.blakebr0.cucumber.helper.NBTHelper;
 import com.blakebr0.pickletweaks.feature.item.MagnetItem;
 import com.blakebr0.pickletweaks.network.NetworkHandler;
 import com.blakebr0.pickletweaks.network.message.ToggleMagnetMessage;
@@ -9,6 +10,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -17,18 +19,25 @@ public final class ToggleMagnetInInventoryHandler {
     public void onMouseClicked(GuiScreenEvent.MouseClickedEvent.Pre event) {
         Minecraft mc = Minecraft.getInstance();
         Screen gui = event.getGui();
+
         if (gui instanceof ContainerScreen<?> && event.getButton() == 1) {
             ContainerScreen<?> container = (ContainerScreen<?>) gui;
             Slot slot = container.getSlotUnderMouse();
             ClientPlayerEntity player = mc.player;
+
             if (player == null)
                 return;
 
             ItemStack held = player.inventory.getItemStack();
+
             if (slot != null && held.isEmpty() && slot.inventory == player.inventory) {
                 ItemStack stack = slot.getStack();
+
                 if (stack.getItem() instanceof MagnetItem) {
                     NetworkHandler.INSTANCE.sendToServer(new ToggleMagnetMessage(slot.slotNumber));
+
+                    player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5F, NBTHelper.getBoolean(stack, "Enabled") ? 0.5F : 1.0F);
+
                     event.setCanceled(true);
                 }
             }
--- src/main/java/com/blakebr0/pickletweaks/feature/item/MagnetItem.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/item/MagnetItem.java
@@ -12,7 +12,9 @@ import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 
@@ -32,9 +34,12 @@ public class MagnetItem extends BaseItem implements IEnableable {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
 		ItemStack stack = player.getHeldItem(hand);
+
+		player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5F, NBTHelper.getBoolean(stack, "Enabled") ? 0.5F : 1.0F);
+
 		NBTHelper.flipBoolean(stack, "Enabled");
 
-		return super.onItemRightClick(world, player, hand);
+		return new ActionResult<>(ActionResultType.SUCCESS, stack);
 	}
 
 	@Override
@@ -48,28 +53,26 @@ public class MagnetItem extends BaseItem implements IEnableable {
 
 	@Override
 	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean isSelected) {
-		if (entity instanceof PlayerEntity) {
-			if (NBTHelper.getBoolean(stack, "Enabled")) {
-				double range = ModConfigs.MAGNET_RANGE.get();
-				List<ItemEntity> items = world.getEntitiesWithinAABB(ItemEntity.class, entity.getBoundingBox().grow(range));
-				for (ItemEntity item : items) {
-					if (!item.isAlive() || NBTHelper.getBoolean(stack, "PreventRemoteMovement"))
-						continue;
+		if (entity instanceof PlayerEntity && NBTHelper.getBoolean(stack, "Enabled")) {
+			double range = ModConfigs.MAGNET_RANGE.get();
+			List<ItemEntity> items = world.getEntitiesWithinAABB(ItemEntity.class, entity.getBoundingBox().grow(range));
+			for (ItemEntity item : items) {
+				if (!item.isAlive() || NBTHelper.getBoolean(stack, "PreventRemoteMovement"))
+					continue;
 
-					if (item.getThrowerId() != null && item.getThrowerId().equals(entity.getUniqueID()) && item.cannotPickup())
-						continue;
+				if (item.getThrowerId() != null && item.getThrowerId().equals(entity.getUniqueID()) && item.cannotPickup())
+					continue;
 
-					if (!world.isRemote()) {
-						item.setNoPickupDelay();
-						item.setPosition(entity.getPosX(), entity.getPosY(), entity.getPosZ());
-					}
+				if (!world.isRemote()) {
+					item.setNoPickupDelay();
+					item.setPosition(entity.getPosX(), entity.getPosY(), entity.getPosZ());
 				}
+			}
 
-				List<ExperienceOrbEntity> xporbs = world.getEntitiesWithinAABB(ExperienceOrbEntity.class, entity.getBoundingBox().grow(range));
-				for (ExperienceOrbEntity orb : xporbs) {
-					if (!world.isRemote()) {
-						orb.setPosition(entity.getPosX(), entity.getPosY(), entity.getPosZ());
-					}
+			List<ExperienceOrbEntity> xporbs = world.getEntitiesWithinAABB(ExperienceOrbEntity.class, entity.getBoundingBox().grow(range));
+			for (ExperienceOrbEntity orb : xporbs) {
+				if (!world.isRemote()) {
+					orb.setPosition(entity.getPosX(), entity.getPosY(), entity.getPosZ());
 				}
 			}
 		}
