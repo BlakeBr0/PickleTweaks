72350e5a6fa751baa4d7a5480931c32385b9a50d
BlakeBr0 <luasmrks@gmail.com>
2021-08-15 14:47:18 -0500

    Use BaseNetworkHandler from cucumber for networking

--- src/main/java/com/blakebr0/pickletweaks/network/NetworkHandler.java
+++ src/main/java/com/blakebr0/pickletweaks/network/NetworkHandler.java
@@ -1,19 +1,14 @@
 package com.blakebr0.pickletweaks.network;
 
+import com.blakebr0.cucumber.network.BaseNetworkHandler;
+import com.blakebr0.pickletweaks.PickleTweaks;
 import com.blakebr0.pickletweaks.network.message.ToggleMagnetMessage;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fmllegacy.network.NetworkRegistry;
-import net.minecraftforge.fmllegacy.network.simple.SimpleChannel;
 
 public class NetworkHandler {
-    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(new ResourceLocation("pickletweaks", "pickletweaks"), () -> "1.0", (s) -> true, (s) -> true);
-    private static int id = 0;
+    public static final BaseNetworkHandler INSTANCE = new BaseNetworkHandler(new ResourceLocation(PickleTweaks.MOD_ID, "main"));
 
     public static void onCommonSetup() {
-        INSTANCE.registerMessage(id(), ToggleMagnetMessage.class, ToggleMagnetMessage::write, ToggleMagnetMessage::read, ToggleMagnetMessage::onMessage);
-    }
-
-    private static int id() {
-        return id++;
+        INSTANCE.register(ToggleMagnetMessage.class, new ToggleMagnetMessage());
     }
 }
--- src/main/java/com/blakebr0/pickletweaks/network/message/ToggleMagnetMessage.java
+++ src/main/java/com/blakebr0/pickletweaks/network/message/ToggleMagnetMessage.java
@@ -1,34 +1,39 @@
 package com.blakebr0.pickletweaks.network.message;
 
 import com.blakebr0.cucumber.helper.NBTHelper;
+import com.blakebr0.cucumber.network.message.Message;
 import com.blakebr0.pickletweaks.feature.item.MagnetItem;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fmllegacy.network.NetworkEvent;
 
 import java.util.function.Supplier;
 
-public class ToggleMagnetMessage {
+public class ToggleMagnetMessage extends Message<ToggleMagnetMessage> {
     private final int slot;
+
+    public ToggleMagnetMessage() {
+        this.slot = -1;
+    }
 
     public ToggleMagnetMessage(int slot) {
         this.slot = slot;
     }
 
-    public static ToggleMagnetMessage read(FriendlyByteBuf buffer) {
+    public ToggleMagnetMessage read(FriendlyByteBuf buffer) {
         return new ToggleMagnetMessage(buffer.readInt());
     }
 
-    public static void write(ToggleMagnetMessage message, FriendlyByteBuf buffer) {
+    public void write(ToggleMagnetMessage message, FriendlyByteBuf buffer) {
         buffer.writeInt(message.slot);
     }
 
-    public static void onMessage(ToggleMagnetMessage message, Supplier<NetworkEvent.Context> context) {
+    public void onMessage(ToggleMagnetMessage message, Supplier<NetworkEvent.Context> context) {
         context.get().enqueueWork(() -> {
-            Player player = context.get().getSender();
+            var player = context.get().getSender();
+
             if (player != null) {
-                ItemStack stack = player.containerMenu.slots.get(message.slot).getItem();
+                var stack = player.containerMenu.slots.get(message.slot).getItem();
+
                 if (stack.getItem() instanceof MagnetItem) {
                     NBTHelper.flipBoolean(stack, "Enabled");
                 }
