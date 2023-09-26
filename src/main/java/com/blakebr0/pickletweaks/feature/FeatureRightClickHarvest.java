4abfdf1ed7fa0dd6af7b4302c0d6bce7de95180f
BlakeBr0 <luasmrks@gmail.com>
2023-09-26 12:03:23 -0500

    Use CropHelper to get seed item in Right Click Harvest Fixes potential log spam and crop incompatibilities

--- src/main/java/com/blakebr0/pickletweaks/feature/FeatureRightClickHarvest.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/FeatureRightClickHarvest.java
@@ -1,25 +1,15 @@
 package com.blakebr0.pickletweaks.feature;
 
-import com.blakebr0.pickletweaks.PickleTweaks;
+import com.blakebr0.cucumber.helper.CropHelper;
 import com.blakebr0.pickletweaks.config.ModConfigs;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
-
-import java.lang.reflect.Method;
 
 public final class FeatureRightClickHarvest {
-	private static final Method GET_SEED;
-
-	static {
-		GET_SEED = ObfuscationReflectionHelper.findMethod(CropBlock.class, "m_6404_");
-	}
-
 	@SubscribeEvent
 	public void onRightClickCrop(RightClickBlock event) {
 		if (!ModConfigs.ENABLE_RIGHT_CLICK_HARVEST.get())
@@ -39,7 +29,7 @@ public final class FeatureRightClickHarvest {
 			var block = state.getBlock();
 
 			if (block instanceof CropBlock crop) {
-				var seed = getSeed(crop);
+				var seed = CropHelper.getSeedsItem(crop);
 
 				if (crop.isMaxAge(state) && seed != null) {
 					var drops = Block.getDrops(state, (ServerLevel) level, pos, level.getBlockEntity(pos));
@@ -63,15 +53,5 @@ public final class FeatureRightClickHarvest {
 				}
 			}
 		}
-	}
-
-	private Item getSeed(Block block) {
-		try {
-			return (Item) GET_SEED.invoke(block);
-		} catch (Exception e) {
-			PickleTweaks.LOGGER.error("Unable to get seed from crop {}", e.getLocalizedMessage());
-		}
-
-		return null;
 	}
 }
