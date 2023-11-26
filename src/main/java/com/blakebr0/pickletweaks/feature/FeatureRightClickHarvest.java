23066bf939a7908a9a7ac45cdec6e3bd2d44a7d8
BlakeBr0 <luasmrks@gmail.com>
2023-11-26 11:24:48 -0600

    Add support for harvesting Nether Wart with Right Click Harvest

--- src/main/java/com/blakebr0/pickletweaks/feature/FeatureRightClickHarvest.java
+++ src/main/java/com/blakebr0/pickletweaks/feature/FeatureRightClickHarvest.java
@@ -2,10 +2,17 @@ package com.blakebr0.pickletweaks.feature;
 
 import com.blakebr0.cucumber.helper.CropHelper;
 import com.blakebr0.pickletweaks.config.ModConfigs;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.NetherWartBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -27,35 +34,46 @@ public final class FeatureRightClickHarvest {
 			var pos = event.getPos();
 			var state = level.getBlockState(pos);
 			var block = state.getBlock();
+			var entity = event.getEntity();
+			var hand = event.getHand();
 
 			if (block instanceof CropBlock crop) {
 				var seed = CropHelper.getSeedsItem(crop);
 
 				if (crop.isMaxAge(state) && seed != null) {
-					var drops = Block.getDrops(state, (ServerLevel) level, pos, level.getBlockEntity(pos));
-
-					for (var drop : drops) {
-						var item = drop.getItem();
-
-						if (!drop.isEmpty() && item == seed) {
-							drop.shrink(1);
-							break;
-						}
-					}
-
-					for (var drop : drops) {
-						if (!drop.isEmpty()) {
-							Block.popResource(level, pos, drop);
-						}
-					}
-
-					var entity = event.getEntity();
-					var hand = event.getHand();
+					handleDrops(state, level, pos, seed);
 
 					entity.swing(hand, true);
 
 					level.setBlockAndUpdate(pos, crop.getStateForAge(0));
 				}
+			}
+
+			if (block instanceof NetherWartBlock && state.getValue(NetherWartBlock.AGE) == 3) {
+				handleDrops(state, level, pos, Items.NETHER_WART);
+
+				entity.swing(hand, true);
+
+				level.setBlockAndUpdate(pos, state.setValue(NetherWartBlock.AGE, 0));
+			}
+		}
+	}
+
+	private static void handleDrops(BlockState state, Level level, BlockPos pos, ItemLike seed) {
+		var drops = Block.getDrops(state, (ServerLevel) level, pos, level.getBlockEntity(pos));
+
+		for (var drop : drops) {
+			var item = drop.getItem();
+
+			if (!drop.isEmpty() && item == seed) {
+				drop.shrink(1);
+				break;
+			}
+		}
+
+		for (var drop : drops) {
+			if (!drop.isEmpty()) {
+				Block.popResource(level, pos, drop);
 			}
 		}
 	}
